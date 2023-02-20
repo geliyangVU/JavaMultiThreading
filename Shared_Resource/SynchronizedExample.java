15ce84910cf2f8aa289902984937f58ccc5aca0e
Geli Yang <gyang5@villanova.edu>
2023-02-20 12:33:56 -0500

    Multithread example results wrong answer.
    2 thread read/write to counter at the same time resulting unwanted final result.
    counter value:4066

---
+++ Shared_Resource/SynchronizedExample.java
@@ -0,0 +1,34 @@
+package Shared_Resource;
+
+public class SynchronizedExample {
+    static int counter = 0;
+
+    public static void main(String[] args) throws InterruptedException {
+        Thread thread1 = new Thread(() -> {
+            // System.out.println("Thread1 start");
+            for (int i = 0; i < 5000; i++) {
+                counter++;
+            }
+
+        }, "thread1");
+
+        Thread thread2 = new Thread(() -> {
+            // System.out.println("Thread2 start");
+            for (int i = 0; i < 5000; i++) {
+                counter--;
+            }
+
+        }, "thread2");
+
+        thread1.start();
+        thread2.start();
+
+        thread1.join();
+        thread2.join();
+        System.out.println("counter value:" + counter);
+
+        System.out.println("Main thread finished");
+
+    }
+
+}
