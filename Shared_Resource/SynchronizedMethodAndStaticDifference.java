b7b18a58e98e3c64b6cb5d7a6c710bfd0bc5fd2c
Geli Yang <gyang5@villanova.edu>
2023-02-20 13:34:20 -0500

    Create SynchronizedMethodAndStaticDifference.java
    synchronized method lock the current object --->
                           synchronized(this)
    synchronized static method will lock the current class-->
                       Synchronized(XXX.class)

---
+++ Shared_Resource/SynchronizedMethodAndStaticDifference.java
@@ -0,0 +1,25 @@
+package Shared_Resource;
+
+public class SynchronizedMethodAndStaticDifference {
+    //synchronized method lock the current object ---> synchronized(this)
+    //synchronized static method will lock the current class--> Synchronized(XXX.class)
+    
+}
+
+
+
+class Room2 {
+    private int counter = 0;
+
+    public synchronized void incrementCounter() {
+        counter++;
+    }
+
+    public synchronized void decrementCounter() {
+        counter--;
+    }
+
+    public synchronized int getCounter() {
+        return counter;
+    }
+}
