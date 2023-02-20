590306f5cb69cb65648aea7737fdfcee78380a05
Geli Yang <gyang5@villanova.edu>
2023-02-19 19:15:32 -0500

    Daemon Thread example code
    An example showing daemon thread will terminate when main thread terminates. Main thread will not wait for the completion for daemon thread

---
+++ DaemonThread.java
@@ -0,0 +1,21 @@
+public class DaemonThread {
+
+    public static void main(String[] args) throws InterruptedException {
+        Thread thread1 = new Thread(() -> {
+            System.out.println("Thread1 start");
+            while (true) {
+                if (Thread.currentThread().isInterrupted()) {
+                    break;
+                }
+            }
+            System.out.println("Thread1 ends");
+
+        }, "thread1");
+
+        thread1.setDaemon(true);
+        Thread.sleep(1000);
+        System.out.println("Main thread ends");
+
+    }
+
+}
