7edfc854eb6237b034279f66efe8255e98870a49
Geli Yang <gyang5@villanova.edu>
2023-03-05 18:18:07 -0500

    Update GuardedObject.java
    Updated GuardedObject class with a time out option for getObject method. Implemented time out check by using waitTime in logic

--- DesignPattern/GuardedObject/GuardedObject.java
+++ DesignPattern/GuardedObject/GuardedObject.java
@@ -20,6 +20,33 @@ public class GuardedObject {
         }
     }
 
+    public Object getObject(long timeout) {
+
+        synchronized (this) {
+            long beginTime = System.currentTimeMillis();
+            long passedTime = 0;
+            while (response == null) {
+
+                long waitTime = timeout - passedTime;// calculate wait time
+
+                if (waitTime <= 0) {
+                    System.out.println("time out occured");
+                    break;
+                }
+
+                try {
+                    this.wait(waitTime);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+
+                passedTime = System.currentTimeMillis() - beginTime;
+            }
+
+            return response;
+        }
+    }
+
     public void generateObject(Object responseToBeGenerated) {
         synchronized (this) {
             this.response = responseToBeGenerated;
