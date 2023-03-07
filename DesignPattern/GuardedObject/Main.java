100ff50ff48ff1afa9d1224fff5792fb8057d13f
Geli Yang <gyang5@villanova.edu>
2023-03-07 13:51:17 -0500

    Implemented test case for GuardedObject design pattern
    Implemented test case for GuardedObject design pattern. Verified Correctness in test

--- DesignPattern/GuardedObject/Main.java
+++ DesignPattern/GuardedObject/Main.java
@@ -5,14 +5,19 @@ public class Main {
         GuardedObject guardedObject = new GuardedObject();
 
         new Thread(() -> {
-            System.out.println("thread 1 wait for response");
-            Object response = guardedObject.getObject();
+            System.out.println("thread 1");
+            Object response = guardedObject.getObject(2000);
             System.out.println(response + "from thread 1");
 
         }, "thread1").start();
 
         new Thread(() -> {
-            System.out.println("worker thread generate response");
+            try {
+                Thread.sleep(3000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+
             guardedObject.generateObject("response is generated");
         }, "workerThread").start();
 
