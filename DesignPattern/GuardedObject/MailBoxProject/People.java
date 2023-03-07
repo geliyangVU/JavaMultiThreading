f00026151a56c5fb290139a09bb8dce08559ec48
Geli Yang <gyang5@villanova.edu>
2023-03-07 14:43:45 -0500

    Implement People class which extends Thread Class
    Override run method to simulate the behavior of register mailbox and pickup mails.

---
+++ DesignPattern/GuardedObject/MailBoxProject/People.java
@@ -0,0 +1,19 @@
+package DesignPattern.GuardedObject.MailBoxProject;
+
+public class People extends Thread {
+    @Override
+    public void run() {
+
+        GuardedObjectForMail guardedObjectForMail = MainBoxes.createGuardedObjectForMail();
+        System.out.println("People wants to get mail id:");
+        System.out.println(guardedObjectForMail.getId());
+
+        Object mailResponse = guardedObjectForMail.getObject(5000);
+        System.out.println("Received mail id:");
+        System.out.println(guardedObjectForMail.getId());
+        System.out.println(mailResponse);
+        System.out.println("Done");
+
+    }
+
+}
